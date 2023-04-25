26e825f8a40ed870be0ca2c3ab99b0b6bf157af0
Duncan-McD <dam342@cornell.edu>
2023-04-25 16:26:14 -0400

    finished an overcrowded plot

--- main.py
+++ main.py
@@ -67,7 +67,8 @@ This is chatgpt Code: DO NOT TRUST
 '''
 import requests
 from plot_ping import generate_plots
-from datetime import datetime, tzinfo
+from datetime import datetime, tzinfo,timedelta
+import random
 
 prefix = 'https://atlas.ripe.net/api/v2/'
 
@@ -79,6 +80,8 @@ DAYS_OF_REQUESTS = 1
 MIN_BETWEEN_REQUESTS = 15
 S_BETWEEN_REQUESTS = MIN_BETWEEN_REQUESTS * 60
 MS_BETWEEN_REQUESTS = S_BETWEEN_REQUESTS * 1000
+
+TEST_GRAPHING = True
 
 def get_starlink_probe_ids():
     params = dict()
@@ -95,19 +98,36 @@ def get_starlink_probe_ids():
         ids.append(res['id'])
     return ids
 
-def generate_fake_time_data(num_probes,start_time):
-    print("GENERATING FAKE TIME DATA")
-    datetime().now(tz=tzinfo(0))
+def generate_fake_time_data(start_time):
+    times = []
+    d = datetime.now()
+    for j in range(start_time, start_time+(int)(HOURS_IN_DAY*MIN_IN_HOUR/MIN_BETWEEN_REQUESTS)):
+        times.append(datetime(d.year,d.month, d.day)+timedelta(0,0,0,0,(15*j)))
+    return times
 
-def generate_fake_latency_data(num_probes,start_time):
-    print("GENERATING FAKE LATENCY DATA")
+
+def generate_fake_latency_data(start_time):
+    data = []
+    for j in range(start_time,start_time+(int)(HOURS_IN_DAY*MIN_IN_HOUR/MIN_BETWEEN_REQUESTS)):
+        data.append(random.randint(0, 200))
+    return data
+
+def test_plots_with_fake_data(num_probes):
+    data = []
+    for i in range(num_probes):
+        fake_time_data = generate_fake_time_data(0)
+        fake_latency_data = generate_fake_latency_data(0)
+        d = [(fake_time_data[i],fake_latency_data[i]) for i in range(0, len(fake_time_data))]
+        data.append(d)
+    generate_plots(data)
 
 def main():
-    starlink__probe_ids = get_starlink_probe_ids()
-    num_starlink_probes = len(starlink__probe_ids)
-
-    fake_time_data = generate_fake_time_data(num_starlink_probes, 0)
-    fake_latency_data = generate_fake_latency_data(num_starlink_probes, 0)
-    generate_plots(range(1,6), [12.4,6.2,5.3,4.1,8.0])
+    starlink_probe_ids = get_starlink_probe_ids()
+    num_starlink_probes = len(starlink_probe_ids)
+    print("Starlink Probe IDs for " + str(num_starlink_probes) + " Probes:")
+    print(starlink_probe_ids)
+    if TEST_GRAPHING:
+        test_plots_with_fake_data(num_starlink_probes)
+    
 
 main()
