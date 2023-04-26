aa7d2046f84ac5daed2b43ca3191776b4d98959e
Kenneth Cula <kc874@cornell.edu>
2023-04-25 21:46:07 -0400

    Sagan works now. Creating and writing into file

--- sagan.py
+++ sagan.py
@@ -1,95 +1,45 @@
-'''
-ripe.atlas.cousteau library: 
-This is a Python library provided by RIPE Atlas that provides a convenient 
-interface for working with the RIPE Atlas API. You can use it to set up and 
-execute ping measurements, and retrieve the results.
+import requests,json, node
+from ripe.atlas.sagan import PingResult
+source = "https://atlas.ripe.net/api/v2/measurements/52637483/results/"
+response = requests.get(source)
+result = json.loads(response.text)
 
-MeasurementRequest() method: 
-This method allows you to create a new measurement request, which you can 
-use to define the parameters of the ping measurement you want to perform. 
-You can specify the target IP address or hostname, the number of packets 
-to send, the interval between packets, and other parameters.
+#dictionary of probes
+probe_dict = {}
 
-AtlasSource() method: 
-This method allows you to specify the source of the ping measurement. 
-You can specify a single RIPE Atlas probe, a set of probes, or a group of probes.
+#Loop through list of dictionaries
+for dict in result:
+   #Find ping results
+    ping_result = PingResult(dict)
 
-AtlasCreateRequest() method: 
-This method allows you to create a new measurement request using the 
-parameters you have specified.
+    #Collect data
+    probe_id = dict['prb_id']
+    med = ping_result.rtt_median
+    avg = ping_result.rtt_average
+    min = ping_result.rtt_min
+    max = ping_result.rtt_max
+    timestamp = dict['timestamp']
 
-AtlasResultsRequest() method: 
-This method allows you to retrieve the results of a measurement request. 
-You can use it to get the ping times for each packet, as well as other 
-information such as the number of packets lost.'''
+    #Organize into node
+    data = node.DataNode(med,avg,min,max,timestamp)
 
-'''
-from ripe.atlas.cousteau import AtlasSource, AtlasCreateRequest, AtlasResultsRequest
-
-# Set up the measurement parameters
-ping_count = 10
-target_address = "example.com"
-
-# Set up the source of the measurement
-atlas_source = AtlasSource(type="probes", value="US")
-
-# Create the measurement request
-ping_request = AtlasCreateRequest(
-    start_time=None,
-    key=None,
-    measurements=[{
-        "type": "ping",
-        "af": 4,
-        "target": target_address,
-        "description": "Ping measurement",
-        "interval": 300,
-        "packets": ping_count,
-    }],
-    sources=[atlas_source],
-    is_oneoff=True
-)
-
-# Submit the measurement request and get the measurement ID
-ping_response = ping_request.create()
-measurement_id = ping_response["measurements"][0]
-
-# Wait for the measurement to complete
-while True:
-    is_completed = AtlasResultsRequest(msm_id=measurement_id).create().is_success()
-    if is_completed:
-        break
-
-# Get the results of the measurement
-ping_results = AtlasResultsRequest(msm_id=measurement_id).create()
-ping_times = [result["result"]["rtt"] for result in ping_results]
-
-This is chatgpt Code: DO NOT TRUST
-'''
-import requests
-from ripe.atlas.sagan import PingResult, SslResult
+    #checks if probe_id already has list of data
+    if probe_dict.get(probe_id):
+        probe_dict[probe_id].append(data)
+    else:
+        probe_dict[probe_id] = [data]
 
 
-my_result = PingResult('this is where your big JSON blob goes')
 
-my_result.rtt_average
-my_result.rtt_median
-# Returns 123.456
+#Check each datanode for each probe
+for key in probe_dict.keys():
+    for value in probe_dict[key]:
+        print(str(key) + ' // ' + str(value))
 
-
- 
-
-
-source = "https://atlas.ripe.net/api/v1/measurement-latest/1012449/"
-response = requests.get(source).json
-
-for probe_id, result in response.items():
-
-    result = result[0]                 # There's only one result for each probe
-    parsed_result = SslResult(result)  # Parsing magic!
-
-    # Each SslResult has n certificates
-    for certificate in parsed_result.certificates:
-        print(certificate.checksum)  # Print the checksum for this certificate
-
-    # Make use of the handy get_checksum_chain() to render the checksum of each certificate into one string if you want
-    print(parsed_result.get_checksum_chain())
+#Open and write into a file
+output = open("probe_data", "a")
+for key in probe_dict.keys():
+    for value in probe_dict[key]:
+        output.write(str(key) + ' // ' + str(value))
+        output.write("\n")
+output.close()
